fa3c11506f2e650fbb5b04c006c1355165c122aa
Andres Mena <ballerlikemike23@gmail.com>
2019-09-01 20:26:49 -0400

    Setting up 2 routes, home & about

--- src/About.tsx
+++ src/About.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 
-interface Props {}
+interface Props { }
 
 export const About: React.FC<Props> = () => {
-  const postId = 5;
-  return (
-    <div>
-      <div>about</div>
-      <Link to={`/posts/${postId}`}>go to post 1</Link>
-    </div>
-  );
+  return <div>About</div>
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Home } from "./Home";
-import { About } from "./About";
-import { Post } from "./Post";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from './Home'
+import { About } from './About'
 
 // setup
 // create routes
@@ -12,12 +11,8 @@ import { Post } from "./Post";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/posts/:id" exact component={Post} />
-        <Route path="/" render={() => <div>404</div>} />
-      </Switch>
+      <Route path='/' component={Home} />
+      <Route path='/about' component={About} />
     </BrowserRouter>
   );
 };
